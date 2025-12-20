cf5d30f3c2c355b2e289f4722e48d7b883389621
Arius-Cr <arius-cr@qq.com>
2025-12-20 22:33:11 +0800

    添加 FORCE_DEBUG 宏以便在 Release 配置下强制启用调试

--- source/blender/blenkernel/BKE_global.hh
+++ source/blender/blenkernel/BKE_global.hh
@@ -271,10 +271,6 @@ enum {
 
   G_DEBUG_GHOST = (1 << 24),  /* Debug GHOST module. */
   G_DEBUG_WINTAB = (1 << 25), /* Debug Wintab. */
-
-#if defined(_DEBUG) && !defined(_NDEBUG_IME)
-  G_DEBUG_IME = (1 << 31), /* IME (Input Method Editor) messages */
-#endif
 };
 
 #define G_DEBUG_ALL \
